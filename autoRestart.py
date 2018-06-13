4cc12830cde7d96f3a58e0f956ee242177208b92
Bowen Chen <chenbowen9612@gmail.com>
2018-06-13 08:40:20 -0400

    Add files via upload

--- autoRestart.py
+++ autoRestart.py
@@ -4,10 +4,11 @@ import time
 
 while True:
     start = time.time()
-    os.system('gcloud compute instances start instance-1 > startResult.txt')
+    os.system('gcloud compute instances start instance-1')
     end = time.time()
+    time.sleep(20)
     if end - start > 13:
-        os.system('gcloud compute ssh chenbowen9612@instance-1 --command="source startup.txt > trainLog.txt')
+        os.system('gcloud compute ssh chenbowen9612@instance-1 --command="source startup.txt')
     time.sleep(600)
 #os.system('gcloud compute instances start instance-1 & gcloud compute ssh chenbowen9612@instance-1 --command="bash"')
 #os.system('gcloud compute instances start instance-1 & gcloud compute ssh chenbowen9612@instance-1 --command="source startup.txt"')
